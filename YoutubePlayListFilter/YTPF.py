c05b8324c8521d65cc447dd559f4f133a5042535
Gerokop <Art.msc2012@gmail.com>
2018-09-20 18:57:43 +0300

    0.3

--- YoutubePlayListFilter/YTPF.py
+++ YoutubePlayListFilter/YTPF.py
@@ -1,42 +1,37 @@
 import re
 import urllib.request
+from bs4 import BeautifulSoup
+import string
 
 class Program():
 
     pls = []
+    unused = []
 
     def __init__(self):
 
         url = input('Вставьте ссылку на плейлист: ')
-
         url = 'https://www.youtube.com/playl\
 ist?list=PLGxKKdc3OAhhu2vAPgUHzSTJOYXLBKMf6'
-
         pl = PlayList(url)
-
         Program.pls.append(pl)
-
+        
+        for i in range(0,100):
+            i = str(i)
+            for a in list(string.ascii_lowercase):
+                Program.unused.append(r'\x'+i)
+                Program.unused.append(r'\x'+a+i)
+                Program.unused.append(r'\x'+a+a+i)
+                Program.unused.append(r'\x'+a+a)
         YTScraper().scrapeandadd(Program.pls[0].url)
         
 
 class Video():
 
-    def __init__(self,url):
+    def __init__(self,name,url):
 
+        self.name = name
         self.url = url
-        html = urllib.request.urlopen(self.url)
-        html = html.read()
-        html = str(html)
-        print(html)
-        find = re.findall('<h1.*?h1>',html,re.IGNORECASE)
-        print(find)
-        find = '\n'.join(find)
-        find = re.findall('<yt.*?string>',find,re.IGNORECASE)
-        print(find)
-        find = '\n'.join(find)
-        find = re.findall('>.*?<',find,re.IGNORECASE)
-        print(find)
-        self.name = find[0][1:-1]
 
     def __repr__(self):
         return self.name
@@ -52,16 +47,38 @@ class YTScraper():
 
         html = urllib.request.urlopen(site)
         html = html.read()
-        html = str(html)
-        find = re.findall('href=".*?"',html,re.IGNORECASE)
-        find = ' '.join(find)
-        find = re.findall('".*?"',find,re.IGNORECASE)
-        for f in find:
-            if 'list' in f:
-                siteplus = site + '/'
-                f = f[3:-1]
-                f = siteplus + f
-                Program.pls[0].videos.append(Video(f))
+        sp = BeautifulSoup(html, 'html.parser')
+        for tag in sp.find_all('a'):
+            tag = tag.get('href')
+            if 'list' in tag and 'accounts' not in tag:
+                tag = str(tag)
+                tag = 'https://www.youtube.com' + tag
+                html = urllib.request.urlopen(tag)
+                html = html.read()
+                html = str(html)
+                html = html.replace(r'\n','')
+                for i in Program.unused:
+                    if i in html:
+                        html = html.replace(i,'')
+                find1 = re.findall('<h1.*?h1>',html,re.IGNORECASE)
+                print(find1)
+                find1 = '\n'.join(find1)
+                print(find1)
+                find1 = re.findall('">.*?</span>',find1,re.IGNORECASE)
+                print(find1)
+                find1 = '\n'.join(find1)
+                print(find1)
+                find1 = re.findall('>.*?<',find1,re.IGNORECASE)
+                find1 = ''.join(find1)
+                print(find1)
+                find1 = find1[1:-1]
+                find1 = find1.strip()
+                print(tag)
+                print(find1)
+                Program.pls[0].videos.append(Video(find1,tag))
+                print(input())
+            else:
+                continue
                 
 
 class PlayList():
