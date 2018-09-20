3ebacb40fc9f8701010c794e0be4c7db2b10e46f
Gerokop <Art.msc2012@gmail.com>
2018-09-20 19:11:56 +0300

    0.3

--- YoutubePlayListFilter/YTPF.py
+++ YoutubePlayListFilter/YTPF.py
@@ -1,7 +1,6 @@
 import re
 import urllib.request
 from bs4 import BeautifulSoup
-import string
 
 class Program():
 
@@ -15,15 +14,8 @@ class Program():
 ist?list=PLGxKKdc3OAhhu2vAPgUHzSTJOYXLBKMf6'
         pl = PlayList(url)
         Program.pls.append(pl)
+        YTScraper(url).scrapeandadd()
         
-        for i in range(0,100):
-            i = str(i)
-            for a in list(string.ascii_lowercase):
-                Program.unused.append(r'\x'+i)
-                Program.unused.append(r'\x'+a+i)
-                Program.unused.append(r'\x'+a+a+i)
-                Program.unused.append(r'\x'+a+a)
-        YTScraper().scrapeandadd(Program.pls[0].url)
         
 
 class Video():
@@ -39,13 +31,14 @@ class Video():
 
 class YTScraper():
 
-    def __init__(self):
-        pass
+    def __init__(self,site):
+
+        self.site = site
 
 
-    def scrapeandadd(self,site):
+    def scrapeandadd(self):
 
-        html = urllib.request.urlopen(site)
+        html = urllib.request.urlopen(self.site)
         html = html.read()
         sp = BeautifulSoup(html, 'html.parser')
         for tag in sp.find_all('a'):
@@ -57,26 +50,15 @@ class YTScraper():
                 html = html.read()
                 html = str(html)
                 html = html.replace(r'\n','')
-                for i in Program.unused:
-                    if i in html:
-                        html = html.replace(i,'')
                 find1 = re.findall('<h1.*?h1>',html,re.IGNORECASE)
-                print(find1)
                 find1 = '\n'.join(find1)
-                print(find1)
                 find1 = re.findall('">.*?</span>',find1,re.IGNORECASE)
-                print(find1)
                 find1 = '\n'.join(find1)
-                print(find1)
                 find1 = re.findall('>.*?<',find1,re.IGNORECASE)
                 find1 = ''.join(find1)
-                print(find1)
                 find1 = find1[1:-1]
                 find1 = find1.strip()
-                print(tag)
-                print(find1)
                 Program.pls[0].videos.append(Video(find1,tag))
-                print(input())
             else:
                 continue
                 
